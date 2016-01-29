ae99e97715e1ae5295a584f747bbf448268095ca
Shrenil Patel <patel.shrenil@gmail.com>
2016-01-29 11:27:44 -0500

    10,000 freq file

--- main.py
+++ main.py
@@ -13,11 +13,14 @@ class word:
         self.name = name
         self.score = 0
 
-current = "cat"
+current = "SOAK"
+compre = False
 
 def getFreq(w):
+    #if w not in freqlist:
+    #    return 0
     try:
-        return (5000-freqlist.index(w))/3000
+        return (10000-freqlist.index(w))/8000
     except:
         return 0
 
@@ -42,11 +45,15 @@ def main():
             except:
                 pass
 
-    with open('pron.txt') as f:
+    searchbook = 'pocket_proun.txt'
+    if compre:
+        searchbook = 'pron.txt'
+
+    with open(searchbook) as f:
         for l in f.read().splitlines():
             try:
-                ls = ''.join([i for i in grps.group(1).lower() if not i.isdigit()]).replace("(","").replace(")","")
                 grps = re.search(find, l)
+                ls = ''.join([i for i in grps.group(1).lower() if not i.isdigit()]).replace("(","").replace(")","")
                 if ls != "":
                     s = grps.group(2).split(" ")
                     w = word(ls,s)
@@ -57,9 +64,11 @@ def main():
     print c_list
 
     per = wordCompare(c_list,c_list)
+
     for m in map:
-        if (wordCompare(c_list,m.list)/per >= 0.5) and m.name != cur:
-            m.score = wordCompare(c_list,m.list)/per
+        wscore = wordCompare(c_list,m.list)/per
+        if wscore >= 0.5:
+            m.score = wscore
             retlist.append(m)
 
     retlist.sort(key=lambda x: x.score+getFreq(x.name), reverse=True)
@@ -67,41 +76,56 @@ def main():
         print m.name + " -> " + "{0:.0f}%".format(100*wordCompare(c_list,m.list)/per)
 
 
-def getPoints(l,w2,id):
-    if l == w2[id]:
-        return 1
-    elif ''.join([i for i in l if not i.isdigit()]) == ''.join([i for i in w2[id] if not i.isdigit()]):
+def getPoints(l,w2,idd):
+    L = l.strip()
+    W = w2[idd].strip()
+    if L == W:
+        return 1.00
+    elif ''.join([i for i in L if not i.isdigit()]) == ''.join([i for i in W if not i.isdigit()]):
         return 0.65
-    elif l[0] == w2[id][0]:
+    elif L[0] == W[0]:
         return 0.35
-    return 0
+    return 0.0
 
 def wordCompare(word1,word2):
-
+    maxstrikes = 2
     p1 = 0
     p2 = 0
     ln1 = len(word1)
     ln2 = len(word2)
-
+    strikes = 0
 
     idx = 0
     for l in word1:
         if idx > ln2-1:
             break
-
-        p1 += getPoints(l,word2,idx)
+        gp = getPoints(l,word2,idx)
+        if gp == 0:
+            strikes += 1
+            if strikes == maxstrikes:
+                return 0
+        else:
+            strikes = 0
+        p1 += gp
         idx += 1
 
     idx = 0
+    strikes = 0
     for l in word1:
         id = ln2-ln1+idx
-        if id > ln2-1 or id < 0:
+        if id > ln2 or id < 0:
             break
-
-        p2 += getPoints(l,word2,id)
+        gp = getPoints(l,word2,id)
+        if gp == 0:
+            strikes += 1
+            if strikes == maxstrikes:
+                return 0
+        else:
+            strikes = 0
+        p2 += gp
         idx += 1
 
-    return max([p1,p2])
+    return max(p1,p2)
 
 if __name__ == "__main__":
     main()
